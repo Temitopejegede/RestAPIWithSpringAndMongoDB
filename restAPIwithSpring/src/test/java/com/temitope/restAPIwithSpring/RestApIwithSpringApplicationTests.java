2d2726ea1b2e9138cfe3f3a9ae64a7fd633050ba
Temitope Jegede <temiteejegede@gmail.com>
2022-06-17 23:44:39 -0400

    Initial commit

---
+++ restAPIwithSpring/src/test/java/com/temitope/restAPIwithSpring/RestApIwithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temitope.restAPIwithSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApIwithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
